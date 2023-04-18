5350bf359454b1cc1de9c4c24d151378836030f6
dinh <dinh.vu0204@gmail.com>
2023-04-18 13:40:34 +0200

    Modified LoginView

--- htp/src/views/LoginView.js
+++ htp/src/views/LoginView.js
@@ -1,18 +1,73 @@
 import React from "react";
+import "./Styled.css";
 
-const LoginView = () => {
+const LoginView = ({
+  isLoggedIn,
+  email,
+  password,
+  confirmPassword,
+  error,
+  isRegistering,
+  setEmail,
+  setPassword,
+  setConfirmPassword,
+  handleSubmit,
+  handleToggle,
+}) => {
   return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" required />
+    <div className="login-container">
+      {isLoggedIn ? (
+        <p>You are now logged in!</p>
+      ) : (
+        <>
+          <h1 className="login-header">
+            {isRegistering ? "Register" : "Login"}
+          </h1>
+          {error && <p className="login-error">{error}</p>}
+          <form className="login-form" onSubmit={handleSubmit}>
+            <label className="login-label">
+              <input
+                className="login-input"
+                type="email"
+                value={email}
+                placeholder="Email:"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label className="login-label">
+              <input
+                className="login-input"
+                type="password"
+                value={password}
+                placeholder="Password:"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
 
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required />
-
-        <button type="submit">Submit</button>
-      </form>
+            {isRegistering && (
+              <label className="login-label">
+                <input
+                  className="login-input"
+                  type="password"
+                  value={confirmPassword}
+                  placeholder="Confirm Password:"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </label>
+            )}
+            <button className="login-button" type="submit">
+              {isRegistering ? "Register" : "Login"}
+            </button>
+            <button
+              className="login-toggle-button"
+              type="button"
+              onClick={handleToggle}
+            >
+              {isRegistering ? "Go back to Login" : "Register"}
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
