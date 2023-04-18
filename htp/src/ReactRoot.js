475e962a5d17c38ce9dfa671f5aedb55afc0300e
dinh <dinh.vu0204@gmail.com>
2023-04-18 15:38:24 +0200

    Modified

--- htp/src/ReactRoot.js
+++ htp/src/ReactRoot.js
@@ -1,37 +1,39 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
-import "./index.css"
-import "./views/Styled.css"
-import Login from "./presenters/LoginPresenter"
-import MainPage from "./presenters/MainPagePresenter"
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import "./views/Styled.css";
+import Login from "./presenters/LoginPresenter";
+import MainPage from "./presenters/MainPagePresenter";
+import Sidebar from "./components/Sidebar";
+import loggedIn from "./loggedIn";
 
 const router = createBrowserRouter([
   { path: "/", element: <MainPage /> },
   { path: "/login", element: <Login /> },
-])
+]);
 
 function ReactRoot() {
   const homeClickCB = () => {
-    window.location.href = "/"
-  }
+    window.location.href = "/";
+  };
   const loginClickCB = () => {
-    window.location.href = "/login"
-  }
+    window.location.href = "/login";
+  };
 
+  const var_loggedIn = loggedIn();
 
   return (
-    <div>
-      <div className='flexRow tempNav'>
-        <button onClick={homeClickCB}>Home</button>
-        <button onClick={loginClickCB}>Login</button>
-      </div>
-
+    <div style={{ width: "100%" }}>
+      {var_loggedIn ? (
+        <Sidebar />
+      ) : (
+        <div className="flexRow tempNav">
+          <button onClick={homeClickCB}>Home</button>
+          <button onClick={loginClickCB}>Login</button>
+        </div>
+      )}
 
       <RouterProvider router={router} />
     </div>
   );
 }
-export default ReactRoot
+export default ReactRoot;
