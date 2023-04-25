8ee2555adc39d2c50fa0166f481ac10e556b41b4
dinh <dinh.vu0204@gmail.com>
2023-04-25 14:11:47 +0200

    Modified

--- htp/src/views/ProfileView.js
+++ htp/src/views/ProfileView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Styled.css";
 
 const user = {
@@ -8,35 +8,72 @@ const user = {
   imageURL: "",
 };
 
-const ProfileView = () => {
-  function onNameChangeACB(e) {
+const ProfilePageView = () => {
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [email, setEmail] = useState(user.email);
+  const [image, setImage] = useState(user.imageURL);
+  
+  function handleImageUpload(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setImage(reader.result);
+    };
   }
+  
+  function handleSaveChanges() {
+    const updatedUser = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      imageURL: image,
+    };
+    // Save updatedUser to the server or perform any other necessary action
+    console.log(updatedUser);
+  }
+  
   return (
-    <div className="profil-container" style={{ marginTop: "100px" }}>
-      <h1 className="title">Profile Page</h1>
+    <div className="profil-container">
+      <h1 style={{ marginTop: '100px' }}>Profile Page</h1>
       <div className="input-details">
         <div className="profile">
-          <div className="profile-image"></div>
-          <button className="upload-profile-button">Upload</button>
+          <div className="profile-image" style={{backgroundImage: `url(${image})`}}></div>
+          <input type="file" onChange={handleImageUpload} />
         </div>
         <div className="details-container">
           <div>
             <label>Firstname</label>
-            <input placeholder={user.firstName}></input>
+            <input
+              placeholder={firstName}
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div>
             <label>Lastname</label>
-            <input placeholder={user.lastName} onInput={onNameChangeACB}></input>
+            <input
+              placeholder={lastName}
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
           </div>
           <div>
             <label>Email:</label>
-            <input onInput={onNameChangeACB} placeholder={user.email}></input>
+            <input
+              placeholder={email}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
-          <button className="save-button">Save changes</button>
+          <button className="save-button" onClick={handleSaveChanges}>
+            Save changes
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-export default ProfileView;
+export default ProfilePageView;
