8b130b5511790ad0434b4ba37b0445ba21e4939d
Sebastian Rone <89859504+sebgro98@users.noreply.github.com>
2023-04-25 16:48:07 +0200

    Reactroot deleted useless stuff

--- htp/src/ReactRoot.js
+++ htp/src/ReactRoot.js
@@ -1,41 +1,31 @@
-import {useNavigate, Link, Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
 import "./index.css";
 import "./views/Styled.css";
 import Login from "./presenters/LoginPresenter";
 import MainPage from "./presenters/MainPagePresenter";
 import Sidebar from "./components/Sidebar";
 import loggedIn from "./loggedIn";
-import Display from "./presenters/DisplayPresenter";
 
 function ReactRoot() {
-    const navigate = useNavigate();
-    const homeClickCB = () => {
-        navigate("/");
-    };
-    const loginClickCB = () => {
-        navigate("/login");
-    };
-
     const var_loggedIn = loggedIn();
 
     return (
 
-            <div style={{ width: "100%" }}>
-                {var_loggedIn ? (
-                    <Sidebar />
-                ) : (
-                    <div className="flexRow tempNav">
-                        <Link to="/">Home</Link>
-                        <Link to="/login">Login</Link>
-                    </div>
-                )}
+        <div style={{width: "100%"}}>
+            {var_loggedIn ? (
+                <Sidebar/>
+            ) : (
+                <div className="flexRow tempNav">
+                    <Link to="/">Home</Link>
+                    <Link to="/login">Login</Link>
+                </div>
+            )}
 
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/display" element={<Display />} />
-                </Routes>
-            </div>
+            <Routes>
+                <Route path="/" element={<MainPage/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
+        </div>
     );
 }
 
