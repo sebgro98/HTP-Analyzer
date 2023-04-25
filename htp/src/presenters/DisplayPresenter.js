1d09d3dbbcbef34db01995a4fe64616279a2cea3
dinh <dinh.vu0204@gmail.com>
2023-04-25 13:33:32 +0200

    Modified

--- htp/src/presenters/DisplayPresenter.js
+++ htp/src/presenters/DisplayPresenter.js
@@ -1,34 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import Model from "../Model";
+import Model from '../Model';
 import DisplayView from '../views/DisplayView';
 
+function DisplayPresenter({ darkMode }) {
+  const [data, setData] = useState(null);
 
-function DisplayPresenter({darkMode}) {
-    const [data, setData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const model = new Model();
+      console.log('Fetching data...');
+      const result = await model.retrieveDataForEmail('hej@gmail.com');
+      console.log('Data retrieved:', result);
+      setData(result);
+    }
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        async function fetchData() {
-            const model = new Model();
-            console.log('fetching data...');
-            const result = await model.retrieveDataForEmail('hej@gmail.com');
-            console.log('data retrieved:', result);
-            setData(result);
-        }
-        fetchData();
-    }, []);
+  console.log('Data:', data);
 
-    console.log('data:', data);
-
-    return (
-        <div className="DisplayPresenter">
-            <h1>Display Data</h1>
-            {data ? (
-                <DisplayView darkMode = {darkMode} data={data} />
-            ) : (
-                <p>Loading data...</p>
-            )}
-        </div>
-    );
+  return (
+    <div className="DisplayPresenter">
+      {data ? (
+        <DisplayView darkMode={darkMode} data={data} />
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
 }
 
 export default DisplayPresenter;
