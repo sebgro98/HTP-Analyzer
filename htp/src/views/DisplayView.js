020c8ec3afaa038a24742466a7453aebbf40da37
Dinh Vu <dinhv@kth.se>
2023-05-04 01:44:25 +0200

    add graph to display

--- htp/src/views/DisplayView.js
+++ htp/src/views/DisplayView.js
@@ -1,56 +1,130 @@
-import React from 'react';
-import MaxMinPresenter from '../presenters/MaxMinPresenter';
-import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
-import WaterDropIcon from '@mui/icons-material/WaterDrop';
-import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
-import "./Styled.css"
-import { darkModeAtom } from '../views/MainPageView';
-import { useRecoilState } from 'recoil';
+import React, { useState, useEffect } from "react";
+import MaxMinPresenter from "../presenters/MaxMinPresenter";
+import hmdty from "./images/water_2.svg";
+import temp from "./images/temperature-half-svgrepo-com.svg";
+import prs from "./images/pressure-svgrepo-com.svg";
+import Graph from "../components/Graph";
+import "./Styled.css";
+import { db } from "../firebaseModel";
+import { collection, onSnapshot } from "firebase/firestore";
 
+function DisplayView({ darkMode }) {
+  const [data, setData] = useState(null);
+  const { WeatherData, CurrentIntervals } = data || {};
 
-function DisplayView({ data }) {
-  const [darkMode] = useRecoilState(darkModeAtom);
+  const formatGraphData = (data) => {
+    if (!Array.isArray(data)) {
+      return [];
+    }
+    return data.map((item) => ({
+      //date: item.timestamp.toDate().toLocaleString(),
+      value: item.value,
+    }));
+  };
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "Data"), (querySnapshot) => {
+      const data = [];
+      querySnapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+      setData(data[0]);
+    });
+  
+    return () => unsubscribe();
+  }, []);
+  
+  useEffect(() => {
+    if (WeatherData) {
+      console.log("================");
+      console.log("WeatherData:", WeatherData);
+      console.log("Type: humidity");
+      console.log("Formatted data:", formatGraphData(WeatherData.humidity));
+      console.log("Type: temperature");
+      console.log("Formatted data:", formatGraphData(WeatherData.temperature));
+      console.log("Type: pressure");
+      console.log("Formatted data:", formatGraphData(WeatherData.pressure));
+      console.log("================");
+    }
+  }, [WeatherData]);
+
   if (!data) {
     return <p>Loading data...</p>;
   }
-
-  const { WeatherData } = data;
   
   return (
-    <div className={`card${darkMode ? 'Dark' : 'Light'}`}>
-      <div className="column">
-        <div className="box">
-        <WaterDropIcon style={{color: darkMode ? "#ffffff" : "#1a1a1a"}} sx={{ fontSize: 60 }}/>
-          <div className="measurement">Humidity</div>
-          <div className="parameter">
-            <span className="value">{WeatherData.Hum[0]}</span>
-            <span>%</span>
+    <div>
+      <div className={`card${darkMode ? "Dark" : "Light"}`}>
+        <div className="column">
+          <div className="box">
+            <img className="svg-icon" src={hmdty} alt="Humidity" />
+            <div className="measurement">Humidity</div>
+            <div className="parameter">
+              <span className="value">{WeatherData.Hum[0]}</span>
+              <span>%</span>
+            </div>
+            <MaxMinPresenter
+              maxName="CurrentIntervals.HumMax"
+              minName="CurrentIntervals.HumMin"
+              initMax={CurrentIntervals.HumMax}
+              initMin={CurrentIntervals.HumMin}
+            />
+            {Array.isArray(WeatherData.Hum) && WeatherData.Hum.length > 0 && (
+              <Graph
+                data={formatGraphData(WeatherData.Hum)}
+                type="humidity"
+                darkMode={darkMode}
+              />
+            )}
           </div>
-          <MaxMinPresenter maxName = {"CurrentIntervals.HumMax"} minName = {"CurrentIntervals.HumMin"} initMax={100} initMin={12} />
         </div>
-      </div>
 
-      <div className="column">
-        <div className="box">
-          <DeviceThermostatIcon style={{color: darkMode ? "#ffffff" : "#1a1a1a"}} sx={{ fontSize: 60 }}/>
-          <div className="measurement">Temperature</div>
-          <div className="parameter">
-            <span className="value">{WeatherData.Temp[0]}</span>
-            <span>&deg;C</span>
+        <div className="column">
+          <div className="box">
+            <img className="svg-icon" src={temp} alt="Temperature" />
+            <div className="measurement">Temperature</div>
+            <div className="parameter">
+              <span className="value">{WeatherData.Temp[0]}</span>
+              <span>&deg;C</span>
+            </div>
+            <MaxMinPresenter
+              maxName="CurrentIntervals.TempMax"
+              minName="CurrentIntervals.TempMin"
+              initMax={CurrentIntervals.TempMax}
+              initMin={CurrentIntervals.TempMin}
+            />
+            {Array.isArray(WeatherData.Temp) && WeatherData.Temp.length > 0 && (
+              <Graph
+                data={formatGraphData(WeatherData.Temp)}
+                type="temperature"
+                darkMode={darkMode}
+              />
+            )}
           </div>
-          <MaxMinPresenter maxName = {"CurrentIntervals.TempMax"} minName = {"CurrentIntervals.TempMin"} initMax={28} initMin={2} />
         </div>
-      </div>
 
-      <div className="column">
-        <div className="box">
-        <CompareArrowsIcon style={{color: darkMode ? "#ffffff" : "#1a1a1a"}} sx={{ fontSize: 60 }}/>
-          <div className="measurement">Pressure</div>
-          <div className="parameter">
-            <span className="value">{WeatherData.Pres[0]}</span>
-            <span>hPa</span>
+        <div className="column">
+          <div className="box">
+            <img className="svg-icon" src={prs} alt="Pressure" />
+            <div className="measurement">Pressure</div>
+            <div className="parameter">
+              <span className="value">{WeatherData.Pres[0]}</span>
+              <span>hPa</span>
+            </div>
+            <MaxMinPresenter
+              maxName="CurrentIntervals.PresMax"
+              minName="CurrentIntervals.PresMin"
+              initMax={CurrentIntervals.PresMax}
+              initMin={CurrentIntervals.PresMin}
+            />
+            {Array.isArray(WeatherData.Pres) && WeatherData.Pres.length > 0 && (
+              <Graph
+                data={formatGraphData(WeatherData.Pres)}
+                type="pressure"
+                darkMode={darkMode}
+              />
+            )}
           </div>
-          <MaxMinPresenter maxName = {"CurrentIntervals.PresMax"} minName = {"CurrentIntervals.PresMin"} initMax={1200} initMin={800} />
         </div>
       </div>
     </div>
