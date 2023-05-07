9d936d38afd930d8ae18a1d5f4ab0ae7c17701fd
Dinh Vu <dinhv@kth.se>
2023-05-07 16:12:57 +0200

    working  formatGraphData function

--- htp/src/presenters/DisplayPresenter.js
+++ htp/src/presenters/DisplayPresenter.js
@@ -1,33 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Model from "../Model";
-import DisplayView from '../views/DisplayView';
-import { db } from '../firebaseModel';
+import DisplayView from "../views/DisplayView";
+import { db } from "../firebaseModel";
 import { doc, onSnapshot } from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
 
 function DisplayPresenter() {
-    const [data, setData] = useState(null);
-    useEffect(() => {
-        async function fetchData() {
-            const model = new Model();
-            const user = await model.getUser();
-            const docRef = doc(db, "Data", user.email);
-            onSnapshot(docRef, (doc) => {
-                setData(doc.data());
-            })
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const model = new Model();
+        const user = await model.getUser();
+        if (!user) {
+          setError("You need to sign in/register to see your data");
+          return;
         }
-        fetchData();
-    }, []);
+        const docRef = doc(db, "Data", user.email);
+        
+        onSnapshot(docRef, (doc) => {
+          const fetchedData = doc.data();
+          if (!fetchedData?.WeatherData?.Time) {
+            setError("Data is missing or incomplete");
+            return;
+          }
+          setData(fetchedData);
+          setError(null);
+        });
+      } catch (error) {
+        setError("Error fetching data");
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
 
-
-    return (
-        <div className="DisplayPresenter">
-            {data ? (
-                <DisplayView data={data} />
-            ) : (
-                <p className='noData'>You need to sign in/register to see your data</p>
-            )}
-        </div>
-    );
+  const formatGraphData = (data, type) => {
+    if (!data?.[type]?.length || !data?.Time?.length) {
+      return [];
+    }
+  
+    const formattedData = data[type].map((value, index) => {
+      const date = data.Time[index] instanceof Timestamp ? data.Time[index].toDate() : data.Time[index];
+      return {
+        date: date,
+        value: Number(value),
+      };
+    });
+  
+    return formattedData;
+  };
+  
+  return (
+    <div className="DisplayPresenter">
+      {error ? (
+        <p className="noData">{error}</p>
+      ) : (
+        <DisplayView data={data} formatGraphData={formatGraphData} />
+      )}
+    </div>
+  );
 }
 
 export default DisplayPresenter;
