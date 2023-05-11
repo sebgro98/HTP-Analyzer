a652011a8195d9fa2471d9579e678f493f456861
Dinh Vu <dinhv@kth.se>
2023-05-11 02:17:42 +0200

    Modified

--- htp/src/views/AboutView.js
+++ htp/src/views/AboutView.js
@@ -1,60 +1,75 @@
 function AboutView({ onHomeClicked }) {
+    const members = [
+      {
+        name: "Sebastian Rone",
+        university: "Royal Institute of Technology",
+        email: "sebgro@kth.se",
+        image: "aboutCardImage1",
+        developerRole: "Web Developer, Firebase Master",
+      },
+      {
+        name: "Valter Lindeberg",
+        university: "Royal Institute of Technology",
+        email: "vlindbe@kth.se",
+        image: "aboutCardImage5",
+        developerRole: "Scum Master, Hardware engineer",
+      },
+      {
+        name: "William Eriksson",
+        university: "Royal Institute of Technology",
+        email: "werik@kth.se",
+        image: "aboutCardImage3",
+        developerRole: "Web Developer,",
+      },
+      {
+        name: "Ingemar Cederholm",
+        university: "Royal Institute of Technology",
+        email: "ngemarc@kth.se",
+        image: "aboutCardImage4",
+        developerRole: "Hardware Engineer,",
+      },
+      {
+        name: "Samir Alami",
+        university: "Royal Institute of Technology",
+        email: "samirala@kth.se",
+        image: "aboutCardImage2",
+        developerRole: "Web Developer",
+      },
+      {
+        name: "Dinh Vu",
+        university: "Royal Institute of Technology",
+        email: "dinhv@kth.se",
+        image: "aboutCardImage6",
+        developerRole: "Web Developer",
+      },
+      {
+        name: "Liam Battini",
+        university: "Royal Institute of Technology",
+        email: "liamb@kth.se",
+        image: "aboutCardImage7",
+        developerRole: "Designer, Product Owner",
+      },
+    ];
   
-
     return (
+      <div>
+        <div className="aboutTitle">ABOUT US</div>
         <div>
-            <div className="aboutTitle">ABOUT US</div>
-        <div> 
-
-        <div className="aboutContent">
-
-            <div className="aboutCard blurBackground">
-                <div className="aboutCardImage aboutCardImage1"></div>
-                <div className="aboutTitleText">Sebastian Rone</div>
-                <div className="aboutContentText">Computer science student from the Royal Institute of Technology</div>
-                <div className="aboutEmail">sebgro@kth.se</div>
-            </div>
-            <div className="aboutCard blurBackground">
-                <div className="aboutCardImage aboutCardImage5"></div>
-                <div className="aboutTitleText">Valter Lindeberg</div>
-                <div className="aboutContentText">Computer science student from the Royal Institute of Technology</div>
-                <div className="aboutEmail">vlindbe@kth.se</div>
-            </div>
-            <div className="aboutCard blurBackground">
-                <div className="aboutCardImage aboutCardImage3"></div>
-                <div className="aboutTitleText">William Eriksson</div>
-                <div className="aboutContentText">Computer science student from the Royal Institute of Technology</div>
-                <div className="aboutEmail">werik@kth.se</div>
-            </div>
-            <div className="aboutCard blurBackground">
-                <div className="aboutCardImage aboutCardImage4"></div>
-                <div className="aboutTitleText">Ingemar Cederholm</div>
-                <div className="aboutContentText">Computer science student from the Royal Institute of Technology</div>
-                <div className="aboutEmail">ngemarc@kth.se</div>
-            </div>
-            <div className="aboutCard blurBackground">
-                <div className="aboutCardImage aboutCardImage2"></div>
-                <div className="aboutTitleText">Samir Alami</div>
-                <div className="aboutContentText">Computer science student from the Royal Institute of Technology</div>
-                <div className="aboutEmail">samirala@kth.se</div>
-            </div>
-            <div className="aboutCard blurBackground">
-                <div className="aboutCardImage aboutCardImage6"></div>
-                <div className="aboutTitleText">Dinh Vu</div>
-                <div className="aboutContentText">Computer science student from the Royal Institute of Technology</div>
-                <div className="aboutEmail">dinhv@kth.se</div>
-            </div>
-            <div className="aboutCard blurBackground">
-                <div className="aboutCardImage aboutCardImage7"></div>
-                <div className="aboutTitleText">Liam Battini</div>
-                <div className="aboutContentText">Computer science student from the Royal Institute of Technology</div>
-                <div className="aboutEmail">liamb@kth.se</div>
-            </div>
-
+          <div className="aboutContent">
+            {members.map((member, index) => (
+              <div key={index} className="aboutCard blurBackground">
+                <div className={`aboutCardImage ${member.image}`}></div>
+                <div className="aboutTitleText">{member.name}</div>
+                <div className="aboutUniversity">University: {member.university}</div>
+                <div className="aboutDeveloperRole">Role: {member.developerRole}</div>
+                <div className="aboutEmail">{member.email}</div>
+              </div>
+            ))}
+          </div>
         </div>
-    </div>
-        </div>
+      </div>
     );
-}
-
-export default AboutView;
+  }
+  
+  export default AboutView;
+  
