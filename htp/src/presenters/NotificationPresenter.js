e3935d95f431137a8886bb533ad3f77c93937b53
Samir Alami <samiralami132@gmail.com>
2023-05-14 09:20:45 +0200

    solved bugs and added text field

--- htp/src/presenters/NotificationPresenter.js
+++ htp/src/presenters/NotificationPresenter.js
@@ -3,13 +3,44 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.min.css";
 import NotificationView from "../views/NotificationView";import Stack from '@mui/material/Stack';
 import Spinner from 'react-bootstrap/Spinner';
-import { dataAtom } from "./DisplayPresenter";
-import { useRecoilState } from 'recoil';
+import { useState, useEffect } from "react";
+import { db } from "../firebaseModel";
+import { doc, onSnapshot } from "firebase/firestore";
 
 const types = ["success", "info", "warning", "error"];
 
-function NotificationPresenter() {
-    const [data] = useRecoilState(dataAtom);
+
+
+function NotificationPresenter({ model }) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        async function fetchData() {
+          try {
+            const user = await model.getUser();
+            if (!user) {
+              setError("You need to sign in/register to see your data");
+              return;
+            }
+            const docRef = doc(db, "Data", user.email);
+    
+            onSnapshot(docRef, (doc) => {
+              const fetchedData = doc.data();
+              if (!fetchedData?.WeatherData?.Time) {
+                setError("Data is missing or incomplete");
+                return;
+              }
+              setData(fetchedData);
+              setError(null);
+            });
+          } catch (error) {
+            setError("Error fetching data");
+            console.error(error);
+          }
+        }
+        fetchData();
+      }, []);
+    
 
   return (
     <div className="DisplayPresenter">
